9f766ce030c3ffb6645298730b844ecb96fe7b23
felipeRodriguez5 <feliperodriguezroldan2@gmail.com>
2025-12-02 22:34:08 -0300

    Fix: rename navbar.jsx to Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/navbar";
+
 function App() {
   return (
     <div>
+      <Navbar />
       <h1 className="text-3xl font-bold underline">Hello, World!</h1>
     </div>
   )
